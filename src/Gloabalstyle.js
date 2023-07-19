64058136031c39c09da7bca42009b091224bb96f
A-Ghoujal <abdogohjal@gmail.com>
2023-07-19 22:35:57 +0100

    change scroll bar width

--- src/Gloabalstyle.js
+++ src/Gloabalstyle.js
@@ -15,7 +15,7 @@ const GlobalStyle = createGlobalStyle`
         color: #6c7983;
         font-size: 1.3rem;
         &::-webkit-scrollbar{
-            width: 7px;
+            width: 9px;
         }
         &::-webkit-scrollbar-thumb{
             background-color: #27AE60;
